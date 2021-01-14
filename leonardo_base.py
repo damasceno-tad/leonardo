776e7df9560171d66bc9b5cc15cd4fd0db0bb9df
Taina D <damasceno.tad@gmail.com>
2021-01-14 16:18:02 +0100

    Testing if the usage of ISA py is correct

--- leonardo_base.py
+++ leonardo_base.py
@@ -14,12 +14,14 @@ import math
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import ISA_leo as isa 
 
 pi = math.pi
 
 # Environment
 gravity = 9.81      # m/s2
 air_density = 1.0   # kg/m3 #TO DO: function of H, using ISA
+isa.atmosphere_setup()
 
 # Launcher          # TODO: make this an objec
 launcher_empty_mass = 10000.0   # kg
@@ -38,32 +40,13 @@ motor_thrust = gravity * motor_Isp * motor_rate     # N
 # Satellite
 satellite_mass = 10.0 # kg
 
-## TODO: create simulation with odeint
-
-def dmass(y, t, mass_rate):
-    if y > 0:
-        dmdt = -mass_rate
-    else:
-        dmdt = 0
-        
-    return dmdt
-
-t = np.arange(0, 105+1, 1)
-
-mass_init = launcher_propl_mass
-mass = odeint(dmass, mass_init, t, args = (motor_rate,))
-# plt.plot(t, mass)
-# plt.axis([90, 105, -5, 5])
-# plt.grid(True)
-# print(mass.min())
-
-
 # Simplified simulation
 
 def drag(x_vel, QnoVxS, d_coeff):
     D = QnoVxS * pow(x_vel, 2.0) * d_coeff
     return D
     
+
 
 def flight(y, t, data):
     alt =  y[0]
@@ -75,6 +58,13 @@ def flight(y, t, data):
     qs        = data[2]    
     cd        = data[3]
     g         = data[4] 
+    
+        
+    T = isa.get_temperature(alt)
+    p = isa.get_pressure(alt)
+    rho = isa.density(T, p, isa.R)
+    print(rho)
+   
     
     drag_force = drag(vel, qs, cd)
     accel = (thrust - drag_force)/mass - g
@@ -92,6 +82,10 @@ def flight(y, t, data):
 
 
 t_f = np.arange(0, 40+0.1, 0.1)
+
+
+# TODO use total mass and differentiate propellant vs total mass in simul
+mass_init = launcher_propl_mass
 
 qnvs = 0.5 * air_density * launcher_surface
 fdata = odeint(flight, [0,0,mass_init], t_f,
